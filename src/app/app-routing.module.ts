7eaf5aa8eaf4cd97456ece9fcaf7fb3db67412c8
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-16 06:17:02 +0200

    Add sidebar with navigations includes toggle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductTrashComponent } from './product-trash/product-trash.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/product-list",
+    pathMatch: "full",
+  },
+  {
+    path: "product-list",
+    component: ProductListComponent
+  },
+  {
+    path: "detail",
+    component: ProductDetailComponent
+  },
+  {
+    path: "trash",
+    component: ProductTrashComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
