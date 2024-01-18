65bb7416459ff54a15e1b8463ae029a3bf9e619c
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 20:40:08 +0200

    feat: removed product add/update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductAddUpdateComponent } from './product-add-update/product-add-update.component';
 import { ProductTrashComponent } from './product-trash/product-trash.component';
 import { ProductGlobalSearchComponent } from './product-global-search/product-global-search.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -24,7 +23,6 @@ import { ProductModalComponent } from './product-modal/product-modal.component';
     AppComponent,
     ProductListComponent,
     ProductDetailComponent,
-    ProductAddUpdateComponent,
     ProductTrashComponent,
     ProductGlobalSearchComponent,
     SidebarComponent,
