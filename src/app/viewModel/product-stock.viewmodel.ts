945b0726f0161af8465f0e88fa57f3f00d0775cc
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 12:47:07 +0200

    feat: add product stock model

---
+++ src/app/viewModel/product-stock.viewmodel.ts
@@ -0,0 +1,8 @@
+export class ProductStockViewModel {
+    stockId!: number;
+    productId!: number;
+    productInStock!: string;
+    isDeleted!: boolean;
+    createdAt!: Date;
+    updateAt!: Date;
+}
